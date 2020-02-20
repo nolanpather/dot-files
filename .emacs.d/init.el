23e57390f8fc78728a5368e723f519e04b94ea8d
Nolan Pather <nolan@jemstep.com>
2020-02-20 16:52:57 +0200

    simple emacs config file

---
+++ .emacs.d/init.el
@@ -0,0 +1,25 @@
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+
+(load-theme 'zenburn t)
+(add-to-list 'default-frame-alist
+             '(font . "Source Code Pro-16"))
+
+(projectile-mode +1)
+(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+
+(global-set-key (kbd "C-x g") 'magit-status)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (magit projectile zenburn-theme))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
