6cdcb886425028dc40402ad3df5d86bc710b1e29
Nolan Pather <nolan@jemstep.com>
2020-02-28 16:01:43 +0200

    syntax checking for ts[x] files using eslint

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -5,7 +5,9 @@
 (package-refresh-contents)
 
 (setq my-packages
-      '(exec-path-from-shell
+      '(add-node-modules-path
+	exec-path-from-shell
+	flycheck
 	magit
         markdown-mode
 	projectile
@@ -17,9 +19,22 @@
   (unless (package-installed-p pkg)
     (package-install pkg)))
 
+;; add-node-modules-path
+;; this package is required to find the eslint executable in local installations of node_modules
+(eval-after-load 'typescript-mode '(add-hook 'typescript-mode-hook #'add-node-modules-path))
+(eval-after-load 'web-mode '(add-hook 'web-mode-hook #'add-node-modules-path))
+
 ;; exec-path-from-shell
 (when (memq window-system '(mac ns x))
   (exec-path-from-shell-initialize))
+
+;; flycheck
+(require 'flycheck)
+(add-hook 'after-init-hook #'global-flycheck-mode)
+(setq-default flycheck-disabled-checkers '(javascript-jshint))
+(flycheck-add-mode 'javascript-eslint 'typescript-mode)
+(flycheck-add-mode 'javascript-eslint 'web-mode)
+(add-hook 'web-mode-hook 'javascript-eslint)
 
 ;; magit
 (global-set-key (kbd "C-x g") 'magit-status)
@@ -43,17 +58,20 @@
   (interactive)
   (tide-setup)
   (eldoc-mode +1)
-  (tide-hl-identifier-mode +1))
-(add-hook 'before-save-hook 'tide-format-before-save)
+  (tide-hl-identifier-mode +1)
+  (flycheck-add-next-checker 'typescript-tide 'javascript-eslint 'append))
 (add-hook 'typescript-mode-hook #'setup-tide-mode)
 
-(add-to-list 'auto-mode-alist '("\\.jsx?$" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.tsx?$" . web-mode))
-(setq web-mode-content-types-alist '(("jsx" . "\\.js[x]?\\'")))
+;; web-mode
+(add-to-list 'auto-mode-alist '("\\.tsx" . web-mode))
 (add-hook 'web-mode-hook
 	  (lambda ()
 	    (when (string-equal "tsx" (file-name-extension buffer-file-name))
 	      (setup-tide-mode))))
+
+;; ---------------------------------------------------------------------
+;; the following is generated code - do not add anything past this point
+;; ---------------------------------------------------------------------
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
